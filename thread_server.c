9308d0b09b1206be8092a3605a47d3288e21e474
John Warren <jhewarren@gmail.com>
2018-03-02 18:50:36 -0800

    poll client

--- thread_server.c
+++ thread_server.c
@@ -11,20 +11,13 @@
 #define SERVERPORT 8000
 #define LIMIT 10000
 //gcc thread_server.c lib/socketwrapper.c -lpthread -o thread_server
-
-void exit_signal(int signo)
-{
-    printf("CTRL+C exit");
-    return 1;
-}
-
 void *connection_handler(void *socket_desc)
 {
     //Get the socket descriptor
     int sock = *(int*)socket_desc;
     int read_size,e;
     char *message;
-    int e;
+    //int e;
     //Receive a message from client
     while(1)
     {
@@ -33,54 +26,44 @@ void *connection_handler(void *socket_desc)
 		//Send the message back to client
         if((e = SendMsg(sock , message)) == -1)
 			break;
-		
+
 		//clear the message buffer
 		memset(message, 0, BUFLEN);
-    }         
+    }
     return 0;
-} 
+}
 
 int main(int argc , char *argv[])
 {
-    int socket_desc , client_sock , c;
+    int socket_desc , *client_sock;
     struct sockaddr_in server , client;
-    
-    struct sigaction act;
-    memset(&act,0,sizeof(act));
-	act.sa_handler = &exit_signal;
-	act.sa_flags = 0;
-	if ((sigemptyset (&act.sa_mask) == -1 || sigaction (SIGINT, &act, NULL) == -1))
-	{
-			perror ("Failed to set SIGINT handler");
-			exit (EXIT_FAILURE);
-	}
 
     socket_desc = Socket(AF_INET , SOCK_STREAM , 0);
-    
-	ConfigServerSocket((struct sockaddr *)&server,SERVERPORT);
 
-    Bind(SERVERPORT, (struct sockaddr *)&server);
+	ConfigServerSocket((struct sockaddr_in *)&server,SERVERPORT);
+
+    Bind(socket_desc, (struct sockaddr_in *)&server);
 
     Listen(socket_desc , LIMIT);
 
     //Accept and incoming connection
-    c = sizeof(struct sockaddr_in);
+    client_sock = calloc(1,sizeof(struct sockaddr_in));
 	pthread_t thread_id;
-	
-    while( (client_sock = Accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
+
+    while( (client_sock = Accept(socket_desc, (struct sockaddr *)client, (socklen_t*)client_sock)) )
     {
-        if( pthread_create( &thread_id , NULL ,  connection_handler , (void*) &client_sock) == 0)
+        if( pthread_create( &thread_id , NULL ,  connection_handler , (void*) client_sock) != 0)
         {
             perror("could not create thread");
             return 1;
         }
     }
-     
+
     if (client_sock < 0)
     {
         perror("accept failed");
         return 2;
     }
-     
+
     return 0;
 }
